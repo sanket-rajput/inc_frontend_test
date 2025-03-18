0afd8a35454869fae8deccc5f76f6369e64e8114
piyush960 <piyushdahake096@gmail.com>
2025-03-18 22:37:29 +0530

    add render url

--- src/app/services/judgeAPI.js
+++ src/app/services/judgeAPI.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseURL = ("https://api.pictinc.org") + "/judge";
+const baseURL = ("https://inc-23-website-backend.onrender.com/") + "/judge";
 // const baseURL = "http://localhost:3001/judge";
 
 
